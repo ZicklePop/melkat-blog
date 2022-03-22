0e4f094a7f6304f437f7c67b8cd55f53ffbb6964
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2022-03-22 04:54:01 +0000

    fixing permalinks and canonical urls

--- src/consts/config.js
+++ src/consts/config.js
@@ -1,8 +1,10 @@
 export const pageSize = 25;
 export const description = "a blog by Melanie Kat";
 export const title = "melkat.blog";
+export const baseUrl = "https://melkat.blog"
 
 export default {
+  baseUrl,
   description,
   pageSize,
   title,
