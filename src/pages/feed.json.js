1ddd41196d931942d23a7c0c8bbdf71253d0c00d
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2022-03-24 06:15:57 +0000

    proper json feeding

--- src/pages/feed.json.js
+++ src/pages/feed.json.js
@@ -1,21 +1,51 @@
-import { byDate } from '../utils/sort'
+import { byJsonDate } from '../utils/sort'
+import getTags from '../utils/get-tags'
 
-export async function get() {
-  // TODO: Don't use Astro here since it's not in scope :(
-  let allPosts = Astro.fetchContent('./p/*.md')
-  allPosts = allPosts.filter(o => !o.draft).sort(byDate)
+export async function get() {  let items = await import.meta.glob('./p/*.md')
+  items = await Promise.all(
+    Object.keys(items).map(async el => {
+      let item = items[el]
+      item = await item()
+      const {
+        astro: { html: content_html },
+        cover,
+        date: date_published,
+        draft,
+        id,
+        link: external_url,
+        tags,
+        title,
+      } = item.frontmatter
+        const url = `https://melkat.blog/p/${id}`
+        const image = `https://melkat.blog${cover}`
+
+        return {
+          content_html,
+          date_published,
+          draft,
+          external_url,
+          id: url,
+          image,
+          tags: getTags(tags),
+          title,
+          url,
+        }
+    })
+  )
+  items = items.filter(o => !o.draft).sort(byJsonDate)
 
   return {
-    version: 'https://jsonfeed.org/version/1',
-    title: 'melkat.blog',
-    icon: 'https://melkat.blog/apple-touch-icon.png',
-    home_page_url: 'https://melkat.blog/',
-    feed_url: 'https://melkat.blog/feed.json',
-    items: allPosts.map(item => ({
-      id: item.url,
-      url: item.url,
-      content_html: item.content.html,
-      date_published: item.date,
-    })),
+    body: JSON.stringify({
+      version: 'https://jsonfeed.org/version/1',
+      title: 'melkat.blog',
+      icon: 'https://melkat.blog/apple-touch-icon.png',
+      home_page_url: 'https://melkat.blog/',
+      feed_url: 'https://melkat.blog/feed.json',
+      author: {
+        name: 'Melanie Kat',
+        avatar: 'https://melkat.blog/apple-touch-icon.png',
+      },
+      items,
+    }),
   }
 }
