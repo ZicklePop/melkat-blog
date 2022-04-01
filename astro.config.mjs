cdba3f7e72c3c93ddc3e0c70e73d9023c782319e
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2022-04-01 14:13:49 +0000

    integrations update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 // @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-  devOptions: {
-    hostname: '0.0.0.0',
-    tailwindConfig: './tailwind.config.cjs',
-  },
-	buildOptions: {
-		site: 'https://melkat.blog/',
-	},
-})
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
+export default defineConfig(
+  /** @type {import('astro').AstroUserConfig} */
+  {
+    devOptions: {
+      hostname: "0.0.0.0",
+    },
+    buildOptions: {
+      site: "https://melkat.blog/",
+    },
+    integrations: [
+      tailwind({
+        config: {
+          path: "./tailwind.config.cjs",
+        },
+      }),
+    ],
+  }
+);
