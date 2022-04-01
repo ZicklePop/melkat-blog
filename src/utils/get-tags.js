050292d0003b8dbc87e651817f59d4e266694b40
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2022-04-01 08:30:31 -0700

    prettier format

--- src/utils/get-tags.js
+++ src/utils/get-tags.js
@@ -1,5 +1,5 @@
-export const cleanTags = (tags) => tags?.map((tag) => tag?.trim());
+export const cleanTags = (tags) => tags?.map((tag) => tag?.trim())
 export const getTags = (tagsString) =>
-  cleanTags(tagsString?.replace(" ", "")?.split(","));
+  cleanTags(tagsString?.replace(' ', '')?.split(','))
 
-export default getTags;
+export default getTags
