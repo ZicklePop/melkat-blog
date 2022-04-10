ad97b7ffce9a47ff2664845d61eb8a666b87f396
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2022-04-10 15:49:58 -0700

    markdown to html parsing

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,5 +1,7 @@
 export const noDrafts = (o) => !o.frontmatter.draft
 
+export const noJsonDrafts = (o) => !o.draft
+
 export const hasType = (o) => !!o.frontmatter.type
 
 export const hasDomain = (o) => !!o.frontmatter.domain
