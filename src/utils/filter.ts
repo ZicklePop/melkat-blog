1fa6336e3331513051d5d169df97d7dfca2dc5c9
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2023-02-12 15:51:08 -0800

    tag and domain list pages

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -1,12 +1,7 @@
 import type { CollectionEntry } from 'astro:content'
-import type { MarkdownInstance } from 'astro'
 
 export const noDrafts = ({ data }: CollectionEntry<'posts'>) => !data.draft
 
-export const noFeedDrafts = (o: { draft: boolean }): boolean => !o.draft
+export const hasLink = ({ data }: CollectionEntry<'posts'>) => !!data.link
 
-export const hasLink = (o: MarkdownInstance<Record<string, any>>): boolean =>
-  !!o.frontmatter.link
-
-export const hasTags = (o: MarkdownInstance<Record<string, any>>): boolean =>
-  !!o.frontmatter.tags
+export const hasTags = ({ data }: CollectionEntry<'posts'>) => !!data.tags
--- src/utils/get-posts.ts
+++ src/utils/get-posts.ts
@@ -1,23 +1,19 @@
+import countKeys from './count-keys'
+import getDomain from './get-domain'
+import type { CollectionEntry } from 'astro:content'
 import { hasLink, hasTags } from './filter'
 import { uniq } from './uniques'
-import getDomain from './get-domain'
-import countKeys from './count-keys'
-import type { MarkdownInstance } from 'astro'
 
 // Domains
 
-export const getAllDomains = (
-  posts: MarkdownInstance<Record<string, any>>[]
-): any[] =>
-  uniq(
-    posts.filter(hasLink).map(({ frontmatter: { link } }) => getDomain(link))
-  )
+export const getAllDomains = (posts: CollectionEntry<'posts'>[]): any[] =>
+  uniq(posts.filter(hasLink).map(({ data: { link } }) => getDomain(link)))
 
 export const getDomainsByCount = (
-  posts: MarkdownInstance<Record<string, any>>[]
+  posts: CollectionEntry<'posts'>[]
 ): { key: string; count: number }[] => {
   const data: { [key: string]: number } = {}
-  posts.forEach(({ frontmatter: { link } }) => {
+  posts.forEach(({ data: { link } }) => {
     if (link) {
       const domain = getDomain(link)
       data[domain] = data[domain] ? data[domain] + 1 : 1
@@ -27,32 +23,27 @@ export const getDomainsByCount = (
 }
 
 export const getPostsByDomain = (
-  posts: MarkdownInstance<Record<string, any>>[],
+  posts: CollectionEntry<'posts'>[],
   domain: string
-): MarkdownInstance<Record<string, any>>[] =>
+): CollectionEntry<'posts'>[] =>
   posts.filter(
-    (o: MarkdownInstance<Record<string, any>>) =>
-      getDomain(o.frontmatter.link) === domain
+    ({ data }: CollectionEntry<'posts'>) => getDomain(data.link) === domain
   )
 
 // Tags
 
-export const getAllTags = (
-  posts: MarkdownInstance<Record<string, any>>[]
-): any[] => {
+export const getAllTags = (posts: CollectionEntry<'posts'>[]): any[] => {
   posts = posts.filter(hasTags)
   let allTags: string[] = []
-  posts.forEach(({ frontmatter: { tags } }) =>
+  posts.forEach(({ data: { tags } }) =>
     tags.forEach((tag: string) => allTags.push(tag))
   )
   return uniq(allTags)
 }
 
-export const getTagsByCount = (
-  posts: MarkdownInstance<Record<string, any>>[]
-) => {
+export const getTagsByCount = (posts: CollectionEntry<'posts'>[]) => {
   const data: { [x: string]: number } = {}
-  posts.forEach(({ frontmatter: { tags } }) => {
+  posts.forEach(({ data: { tags } }) => {
     if (tags) {
       tags.forEach((tag: string | number) => {
         data[tag] = data[tag] ? data[tag] + 1 : 1
@@ -63,7 +54,7 @@ export const getTagsByCount = (
 }
 
 export const getPostsByTag = (
-  posts: MarkdownInstance<Record<string, any>>[],
+  posts: CollectionEntry<'posts'>[],
   tag: string
-): MarkdownInstance<Record<string, any>>[] =>
-  posts.filter(o => o.frontmatter.tags && o.frontmatter.tags.includes(tag))
+): CollectionEntry<'posts'>[] =>
+  posts.filter(({ data }) => data.tags && data.tags.includes(tag))
