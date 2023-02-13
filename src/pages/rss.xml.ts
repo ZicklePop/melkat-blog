93fe520631cb82577a32a4ab0a613264a53ad921
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2023-02-12 16:30:18 -0800

    rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,34 +1,33 @@
+import MarkdownIt from 'markdown-it'
 import rss from '@astrojs/rss'
-import { byRssDate } from '../utils/sort'
-import { noFeedDrafts } from '../utils/filter'
+import sanitizeHtml from 'sanitize-html'
+import { byDate } from '../utils/sort'
+import { getCollection } from 'astro:content'
+import { noDrafts } from '../utils/filter'
 import { title as blogTitle, description, baseUrl } from '../consts/config'
 
+const parser = new MarkdownIt()
+
 export const get = async () => {
-  let items = await import.meta.glob('./p/*.md')
-  items = await Promise.all(
-    Object.keys(items).map(async el => {
-      let item = items[el]
-      item = await item()
-      const content_html = await item.compiledContent()
-      const { cover, date, draft, link: external_url, title } = item.frontmatter
-      const url = `${baseUrl}${item.url}`
-      const coverHtml = `<img src="${baseUrl}${cover}" alt="${title}" /><br />`
-
-      return {
-        description: cover ? `${coverHtml}${content_html}` : content_html,
-        draft,
-        link: url,
-        pubDate: date,
-        title,
-      }
-    })
-  )
-  items = items.filter(noFeedDrafts).sort(byRssDate)
-
+  const posts = await getCollection('posts', noDrafts)
   return rss({
     title: blogTitle,
     description,
     site: baseUrl,
-    items,
+    items: posts.sort(byDate).map(post => ({
+      title: post.data.title,
+      pubDate: post.data.date,
+      link: `${baseUrl}/p/${post.slug}`,
+      content: `${
+        post.data.cover
+          ? '<img src="' +
+            baseUrl +
+            post.data.cover +
+            '" alt="' +
+            post.data.title +
+            '" /><br />'
+          : ''
+      }${sanitizeHtml(parser.render(post.body))}`,
+    })),
   })
 }
