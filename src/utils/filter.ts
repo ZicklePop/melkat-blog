f6ae3e65c3bbb87ee89460e8e75a40ebeca4d87b
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2023-02-12 16:00:48 -0800

    tag and domain index pages

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -2,6 +2,12 @@ import type { CollectionEntry } from 'astro:content'
 
 export const noDrafts = ({ data }: CollectionEntry<'posts'>) => !data.draft
 
+export const hasLinkNoDrafts = ({ data }: CollectionEntry<'posts'>) =>
+  !data.draft && !!data.link
+
+export const hasTagsNoDrafts = ({ data }: CollectionEntry<'posts'>) =>
+  !data.draft && !!data.tags
+
 export const hasLink = ({ data }: CollectionEntry<'posts'>) => !!data.link
 
 export const hasTags = ({ data }: CollectionEntry<'posts'>) => !!data.tags
