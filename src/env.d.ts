4ba142269fe88d2555edf47592c1fb8987118fa3
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2024-03-12 21:01:25 -0700

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,16 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="@astrojs/client" />
-
-type UmamiTracker = {
-  track: {
-    (event: string): Promise<string>
-  }
-}
-
-declare global {
-  interface Window {
-    umami: UmamiTracker
-  }
-}
-
-export {}
